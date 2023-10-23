a9555988b933d367c83596d4cd0e8cf9f7a74ce1
osawa-koki <koki@osawa.dev>
2023-10-23 14:26:48 +0900

    eslint --fix

--- src/functions.ts
+++ src/functions.ts
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 
-export function removeChildren(parent: string | HTMLElement) {
+export function removeChildren (parent: string | HTMLElement): void {
   if (typeof parent === 'string') {
     parent = document.getElementById(parent)!
   }
-  while (parent.firstChild) {
+  while (parent.firstChild != null) {
     parent.removeChild(parent.firstChild)
   }
 }
+
+/* eslint-enable @typescript-eslint/no-non-null-assertion */
